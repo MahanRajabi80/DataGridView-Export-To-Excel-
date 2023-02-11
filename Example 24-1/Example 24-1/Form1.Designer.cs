9607af903015727b2d47501774af5f449051dd91
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 16:51:20 +0330

    Add files via upload

---
+++ Example 24-1/Example 24-1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Example_24_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnData = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(100, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(399, 254);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnData
+            // 
+            this.btnData.Location = new System.Drawing.Point(19, 12);
+            this.btnData.Name = "btnData";
+            this.btnData.Size = new System.Drawing.Size(75, 36);
+            this.btnData.TabIndex = 1;
+            this.btnData.Text = "Data";
+            this.btnData.UseVisualStyleBackColor = true;
+            this.btnData.Click += new System.EventHandler(this.btnData_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(505, 230);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(93, 36);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export To Excel";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 280);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnData);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Example 24-1 (DataGridView Export To Excel)";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnData;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ Example 24-1/Example 24-1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//first add reference
+using Excel = Microsoft.Office.Interop.Excel;
+//---------------------------------------------------------
+namespace Example_24_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //---------------------------------------------------------
+        private void btnData_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 10;
+            dataGridView1.ColumnCount = 10;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    dataGridView1[j - 1, i - 1].Value = i * j;
+                }
+            }
+        }
+        //---------------------------------------------------------
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            var excel = new Excel.Application();
+            excel.Application.Workbooks.Add(true);
+            int ColumnIndex = 0;
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                ColumnIndex++;
+                excel.Cells[1, ColumnIndex] = col.HeaderText;
+            }
+            int rowIndex = 0;
+            string val;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                rowIndex++;
+                ColumnIndex = 0;
+                foreach (DataGridViewColumn col in dataGridView1.Columns)
+                {
+                    ColumnIndex++;
+                    if (row.Cells[ColumnIndex - 1].Value == null)
+                        val = "";
+                    else
+                        val = row.Cells[ColumnIndex - 1].Value.ToString();
+                    excel.Cells[rowIndex + 1, ColumnIndex] = val;
+                }
+            }
+            excel.Visible = true;
+        }
+    }
+}
